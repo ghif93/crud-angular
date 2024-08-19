61151e0a081f10fd3dc947334616c6a5106e7847
ghif93 <fernandogarcia9@outlook.com>
2024-08-19 17:38:49 -0300

    Aula 9

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +9,12 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'front-end' }];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
-  constructor() {}
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {}
 }
